9106a3d63a735735a861f52d559f0b15cb1e53c2
Jamie Bliss <astronouth7303@gmail.com>
2013-10-16 22:24:01 -0400

    Verify checksums

--- pn532/_stream.py
+++ pn532/_stream.py
@@ -2,11 +2,28 @@ import serial
 
 from . import Frame, ACK, NACK, Error
 
+class ChecksumError(IOError):
+	pass
+
+class TimeoutError(IOError):
+	pass
+
 class PN532(object):
 	def __init__(self, port):
 		self.port = port
 		#TODO: Default baud
 		self.serial = serial.serial_for_url(self.port)
+
+	@staticmethod
+	def _verifychecksum(*args):
+		s = 0
+		for thing in args:
+			if isinstance(thing, bytes):
+				s += sum(map(ord, thing))
+			else:
+				s += thing
+		if s != 0:
+			raise ChecksumError()
 
 	def send(self, frame):
 		"""p.send(Frame)
@@ -14,7 +31,7 @@ class PN532(object):
 		"""
 		self.serial.write(frame.towire())
 
-	def raw_get(self):
+	def raw_get(self, timeout=None):
 		"""p.raw_get() -> Frame
 		Reads a frame from the port. Doesn't handle parsing errors (sending
 		NACKs).
@@ -23,12 +40,12 @@ class PN532(object):
 		"""
 		header = ""
 		while "\x00\xFF" not in header:
-			header += self.serial.read()
+			header += self.serial.read(1)
 		# We have the start of a frame, get to work.
 		start = header.index("\x00\xFF")
 		header = header[start:]
 		while len(header) <= 4:
-			header += self.serial.read()
+			header += self.serial.read(1)
 		# Now we have enough to begin parsing it out
 		LEN = header[2]
 		LCS = header[3]
@@ -39,15 +56,17 @@ class PN532(object):
 		elif LEN == 0xFF and LCS = 0xFF:
 			# Extended length
 			while len(header) <= 7:
-				header += self.serial.read()
+				header += self.serial.read(1)
 			LENM, LENL = header[4:6]
 			LEN = LENM * 0xFF + LENL
 			LCS = header[6]
-			#FIXME: Verify length
+			self._verifychecksum(LENM, LENL, LCS)
 		else:
 			# Normal Frame
-			#FIXME: Verify length
+			self._verifychecksum(LEN, LCS)
 		data = self.serial.read(LEN)
+		DCS = self.serial.read(1)
+		self._verifychecksum(data, DCS)
 		TFI = data[0]
 		if TFI in "\xD4\xD5":
 			ccode = data[1]
@@ -57,15 +76,16 @@ class PN532(object):
 			# Error Frame
 			raise Error(TFI)
 
-	def get(self):
+	def get(self, timeout=None):
 		"""p.get() -> Frame
 		Reads a frame from the port. DOES handle parsing errors (sending
 		NACKs).
-
-		FIXME: Timeouts
 		"""
-		#FIXME: handle parsing errors?
-		return self.get()
+		while True:
+			try:
+				return self.raw_get(timeout)
+			except ChecksumError:
+				self.send(NACK())
 
 	def doit(self, frame):
 		self.send(frame)
